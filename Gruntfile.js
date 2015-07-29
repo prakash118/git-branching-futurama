60f9c6b5f3ecd444ae323f89ce3244f845533f30
prakash gurung <prakash.gurung@cohaesus.co.uk>
2015-07-29 17:10:59 +0100

    changed grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,56 @@
+// Gruntfile with the configuration of grunt-express and grunt-open. No livereload yet!
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+  // Load Grunt tasks declared in the package.json file
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-		pkg: grunt.file.readJSON('package.json'),
+  // Configure Grunt 
+  grunt.initConfig({
 
-		connect: {
-			server: {
-				options: {
-					port: 3000,
-					hostname: 'localhost',
-					base: 'public'
-				}
-			}
-		},
+    // grunt-express will serve the files from the folders listed in `bases`
+    // on specified `port` and `hostname`
+    express: {
+      all: {
+        options: {
+          port: 9000,
+          hostname: "0.0.0.0",
+          bases: 'public', // Replace with the directory you want the files served from
+                              // Make sure you don't use `.` or `..` in the path as Express
+                              // is likely to return 403 Forbidden responses if you do
+                              // http://stackoverflow.com/questions/14594121/express-res-sendfile-throwing-forbidden-error
+          livereload: true
+        }
+      }
+    },
 
-		copy: {
-            main: {
-                files:[
-                    {
-                        expand: true,
-                        cwd: 'src/',
-                        src: '*.*',
-                        dest: 'public',
-                    }      
-                ],
-            },
-        },
+    // grunt-watch will monitor the projects files
+    watch: {
+      all: {
+        // Replace with whatever file you want to trigger the update from
+        // Either as a String for a single entry 
+        // or an Array of String for multiple entries
+        // You can use globing patterns like `css/**/*.css`
+        // See https://github.com/gruntjs/grunt-contrib-watch#files
+        files: 'src/index.html',
+        options: {
+          livereload: true
+        }
+      }
+    },
 
+    // grunt-open will open your browser at the project's URL
+    open: {
+      all: {
+        // Gets the port from the connect configuration
+        path: 'http://localhost:<%= express.all.options.port%>'
+      }
+    }
+  });
 
-		watch: {
-			html: {
-                files: '/src/*.html',
-                options: {
-					spawn: false,
-					livereload: true
-				}
-            }
-		}
-		
-	});
-
-	grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-
-	grunt.registerTask('default', ['copy', 'connect', 'watch']);
-
-}
+  // Creates the `server` task
+  grunt.registerTask('server', [
+    'express',
+    'open',
+    'watch'
+  ]);
+};
