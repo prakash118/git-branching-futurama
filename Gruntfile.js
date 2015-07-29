1f71f0b1fd7f0e6d6cd09588aa59331f2d31789b
prakash gurung <prakash.gurung@cohaesus.co.uk>
2015-07-29 16:41:27 +0100

    added to grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,7 +32,10 @@ module.exports = function(grunt) {
 			options: {
 				spawn: false,
 				livereload: true
-			}
+			},
+			html: {
+                files: '/src/*.html'
+            }
 		}
 		
 	});
