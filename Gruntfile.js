dd99b67b8eae7a8eb4a4819edde255716997a849
prakash gurung <prakash.gurung@cohaesus.co.uk>
2015-07-29 16:22:09 +0100

    added grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		connect: {
+			server: {
+				options: {
+					port: 3000,
+					hostname: 'localhost',
+					base: 'public'
+				}
+			}
+		},
+
+		copy: {
+            main: {
+                files:[
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: '*.*',
+                        dest: 'public',
+                    }      
+                ],
+            },
+        },
+
+
+		watch: {
+			options: {
+				spawn: false,
+				livereload: true
+			}
+		}
+		
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['copy', 'connect', 'watch']);
+
+}
